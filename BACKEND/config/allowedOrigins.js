bff96d01c0955724d72fcc5fa063e60d29d96217
pssanjay-2007 <pssanjay2007@gmail.com>
2026-07-05 12:39:51 +0530

    cors error correction

--- BACKEND/config/allowedOrigins.js
+++ BACKEND/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 allowedOrigins = [
-  "http://127.0.0.1:5500",
-  "http://localhost:5500",
-  process.env.FRONTEND_URL,
+	"http://127.0.0.1:5500",
+	"http://localhost:5500",
+	"https://dtube-ekjb.onrender.com",
 ];
 
 module.exports = allowedOrigins;
